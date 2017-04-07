37a373a2a3de5ca8f4af84c53d63236eecf729e3
Ajay Gaur <daiict218@gmail.com>
2017-04-08 00:16:02 +0530

    Resolves login issue

--- services/passport.js
+++ services/passport.js
@@ -17,7 +17,7 @@ var jwtOptions = {
 };
 
 var localStrategy = new LocalStrategy(localOptions, function(email, password, done){
-  User.findOne({email: email}, function(err, user){
+  User.findOne({email: email.toLowerCase()}, function(err, user){
     if(err) { return done(err); }
     if(!user) { return done(null, false); }
     user.comparePassword(password, function(err, isMatch){
